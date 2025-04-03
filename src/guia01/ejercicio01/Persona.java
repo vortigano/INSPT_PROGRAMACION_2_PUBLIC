a30cedb3f084f61960a02487e3b5d255e5d17d0a
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-03 01:14:31 -0300

    G01_E01 Clase Persona con métodos básicos implementados

--- src/guia01/ejercicio01/Persona.java
+++ src/guia01/ejercicio01/Persona.java
@@ -1,5 +1,85 @@
 package guia01.ejercicio01;
 
-public class Persona {
+import java.time.Year;
 
+public class Persona {
+  private String nombre;
+  private String apellido;
+  private int añoDeNacimiento;
+  private static final int AÑO_MINIMO = 1900;
+  
+  // devolver el nombre completo de la persona
+  public String getNombreCompleto()
+  {
+      return (nombre + " " + apellido);
+  }
+  
+  // Mostrar su estado en la consola.
+  public void mostrarEstado()
+  {
+      String borde = "******* Persona [" + this + "] *******";
+      
+      System.out.println(borde);
+      System.out.println("  ESTADO  ");
+      System.out.println("  Nombre              : " + nombre);
+      System.out.println("  Apellido            : " + apellido);
+      System.out.println("  Anyo de Nacimiento  : " + añoDeNacimiento);
+      borde = borde.replaceAll(".", "-");
+      System.out.println(borde);
+  }
+  
+  private int getAñoActual()
+  {
+      return Year.now().getValue();
+  }
+  
+  // Obtener su edad actual
+  public int getEdad()
+  {
+      return (getAñoActual() - añoDeNacimiento);
+  }
+  
+  // Saber si es mayor que una edad dada.
+  public boolean esMayorQue(int edadDada)
+  {
+      return(getEdad()>edadDada);
+  }
+  
+  public void setNombre(String nombre)
+  {
+      if(nombre!=null)
+        this.nombre = nombre;
+      else
+        System.err.println("El nombre ingresado es nulo.");
+  }
+  
+  public void setApellido(String apellido)
+  {
+      if(apellido!=null)
+        this.apellido = apellido;
+      else
+        System.err.println("El apellido ingresado es nulo.");
+  }
+  
+  public void setNombreCompleto(String nombre, String apellido)
+  {
+      //validacion mínima
+      if(nombre!=null)
+        this.nombre   = nombre;
+      else
+        System.err.println("El nombre ingresado es nulo.");  
+      
+      if(apellido!=null)
+        this.apellido = apellido;
+      else
+        System.err.println("El apellido ingresado es nulo.");
+  }
+  
+  public void setAñoDeNacimiento(int añoDeNacimiento)
+  {
+      if(añoDeNacimiento > AÑO_MINIMO && añoDeNacimiento < getAñoActual())
+        this.añoDeNacimiento = añoDeNacimiento;
+      else
+        System.err.println("Año de nacimiento ingresado no válido.");
+  }
 }
