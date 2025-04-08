c894c0142cc009696c9817da25272a4ad18733da
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-08 13:35:18 -0300

    G01_E02 Update corregido con el profesor

--- src/guia01/ejercicio02/Empleado.java
+++ src/guia01/ejercicio02/Empleado.java
@@ -4,13 +4,18 @@ public class Empleado {
     
   public static enum EstadoCivil {SOLTERO, CASADO, DIVORCIADO, VIUDO};
     
+  private final static double PCT_DE_BASE_POR_HIJO_AUMENTO      = 0.03;
+  private final static double PTC_MAX_DE_BASE_POR_HIJO_AUMENTO  = 0.10;
+  private final static double PCT_SOLTERO_DESCUENTO             = 0.04;
+    
   private final int     dni;
   private String        nombre;
   private String        apellido;
   private double        salarioBase;
   private EstadoCivil   estadoCivil;
   private int           cantidadDeHijos;
-  Empleado (int dni, String nombre, String apellido, double salarioBase, EstadoCivil estadoCivil, int cantidadDeHijos)
+  
+  public Empleado (int dni, String nombre, String apellido, double salarioBase, EstadoCivil estadoCivil, int cantidadDeHijos)
   {
       this.dni              = dni;
       this.nombre           = nombre;
@@ -25,27 +30,23 @@ public class Empleado {
     return "Empleado{" + "dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", salarioBase=" + salarioBase + ", estadoCivil=" + estadoCivil + ", cantidadDeHijos=" + cantidadDeHijos + '}';
   }
   
-  private static double pctDeBasePorHijoAumento     = 0.03;
-  private static double pctMaxDeBasePorHijoAumento  = 0.10;
-  private static double pctSolteroDescuento         = 0.04;
-  
   public double obtenerSalarioFinal()
   {
       double  salarioFinal = 0, porcentajeDeSalario = 0;
       
       // calcular porcentaje de salario por hijo
-      porcentajeDeSalario = pctDeBasePorHijoAumento * this.cantidadDeHijos;
+      porcentajeDeSalario = PCT_DE_BASE_POR_HIJO_AUMENTO * this.cantidadDeHijos;
       
       // limitado de porcentaje de salario base basado en hijos
-      if(porcentajeDeSalario> pctMaxDeBasePorHijoAumento)
-        porcentajeDeSalario = pctMaxDeBasePorHijoAumento;
+      if(porcentajeDeSalario> PTC_MAX_DE_BASE_POR_HIJO_AUMENTO)
+        porcentajeDeSalario = PTC_MAX_DE_BASE_POR_HIJO_AUMENTO;
       // salario resultante = salarioBase + 3% de salarioBase por hijo (hasta 10% de salarioBase)
       salarioFinal = this.salarioBase + this.salarioBase * porcentajeDeSalario;
            
       // si es soltero se descuenta 4% del resultante
       if(this.estadoCivil == EstadoCivil.SOLTERO)
       {
-        salarioFinal *= (1 - pctSolteroDescuento);
+        salarioFinal *= (1 - PCT_SOLTERO_DESCUENTO);
       }
       return salarioFinal;
   }
