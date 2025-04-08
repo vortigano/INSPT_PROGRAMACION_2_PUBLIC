d18d1a9bac9f345ced1ebdfb8a39409b261328c0
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-07 23:21:04 -0300

    G01_E01 Agrego el enunciado en la clase principal

--- src/guia01/ejercicio01/Principal.java
+++ src/guia01/ejercicio01/Principal.java
@@ -1,3 +1,15 @@
+/*
+  1) Modelá la clase Persona, la cual posea como atributos nombre, apellido y año de nacimiento.
+    Luego, implementá métodos que permitan:
+    ▪ Devolver el nombre completo de la persona.
+    ▪ Mostrar su estado en la consola.
+    ▪ Obtener su edad actual.
+    ▪ Saber si es mayor que una edad dada.
+    ▪ Cambiar su nombre.
+    ▪ Cambiar su apellido.
+    ▪ Cambiar su nombre y su apellido.
+    Finalmente, instanciá una persona en el método main y probá todos sus métodos.
+*/
 package guia01.ejercicio01;
 
 /**
