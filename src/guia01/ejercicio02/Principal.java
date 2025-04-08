dbea811acf86323eeb26995ff04bf61e64973ae4
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-07 23:26:03 -0300

    G01_E02 Agrego clase principal y enunciado

---
+++ src/guia01/ejercicio02/Principal.java
@@ -0,0 +1,27 @@
+/*
+  2) Desarrollar la clase Empleado, cuyos atributos sean su DNI, nombre, 
+     apellido, salario base, estado civil (soltero, casado, divorciado o viudo)
+     y cantidad de hijos.
+     Se sabe que todos los empleados cobran el salario base más un plus del 3% 
+     de éste por cada hijo, con un tope de hasta 10%. 
+     Del salario resultante debe descontarse un 4% en caso de que el estado civil
+     sea soltero.
+     Desarrollar el método obtenerSalarioFinal() que retorne el valor del 
+     salario del empleado según las reglas enunciadas.
+*/
+package guia01.ejercicio02;
+
+/**
+ *
+ * @author neo64
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
