6e29478a1a72f575b76d999845aa75d4d65cad02
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-13 18:31:14 -0300

    G01_E03 Agrego clase principal vacía

---
+++ src/Principal/java.java
@@ -0,0 +1,16 @@
+package Principal;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
