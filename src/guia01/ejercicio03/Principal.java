50d81a0e7d7bcb71d6a4f7569eb04f5042048129
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-13 19:49:17 -0300

    G01_E03 UPDATE Principal consigna de ejercicio

--- src/guia01/ejercicio03/Principal.java
+++ src/guia01/ejercicio03/Principal.java
@@ -1,3 +1,13 @@
+/*
+  Modelá la clase CuentaBancaria, la cual posea como atributos la clave bancaria
+  uniforme (CBU), el tipo (caja de ahorro o cuenta corriente) y el saldo 
+  (inicialmente en 0). Luego, implementá métodos que permitan:
+  ▪ Obtener el saldo actual.
+  ▪ Depositar dinero en la cuenta (actualizando el saldo).
+  ▪ Extraer dinero de la cuenta (actualizando el saldo). Solo puede quedar en 
+  saldo negativo si es cuenta corriente.
+  ▪ Obtener los últimos 3 dígitos del CBU.
+*/
 package guia01.ejercicio03;
 
 /**
