b4d81121b6735e1971ee39151e60f655ed50a585
vortigano <44713214+vortigano@users.noreply.github.com>
2025-04-18 14:00:19 -0300

    G01_E05 ADD Principal.java

---
+++ src/guia01/ejercicio05/Principal.java
@@ -0,0 +1,33 @@
+/*
+    Modelá la clase Fraccion, la cual posea como atributos numerador y
+    denominador. Luego, implementá métodos que permitan:
+    ▪ Mostrarse en consola, con el formato "numerador/denominador".
+    ▪ Obtener el valor decimal.
+    ▪ Sumarle un entero.
+    ▪ Sumarle una fracción.
+    ▪ Simplificar la fracción.
+    ▪ Saber si la fracción es propia, impropia u aparente.
+    Finalmente, instanciá una fracción en el método main y probá todos sus 
+    métodos.
+
+    Fracciones propias: son aquellas en las que el numerador es menor que el 
+    denominador. Fracciones aparentes: son aquellas cuyo numerador es múltiplo
+    del denominador. Fracciones impropias: son aquellas en las que el numerador
+    es mayor que el denominador, pero no múltiplo.
+*/
+package guia01.ejercicio05;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 05");
+    }
+    
+}
