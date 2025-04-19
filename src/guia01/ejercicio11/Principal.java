6d6b88387c0fc8f42115d0b71be9a12a2a4158c5
vortigano <44713214+vortigano@users.noreply.github.com>
2025-04-19 20:39:38 -0300

    G01_E11 ADD Principal y consigna

---
+++ src/guia01/ejercicio11/Principal.java
@@ -0,0 +1,27 @@
+/*
+  Utilizando las clases generadas hasta ahora, codificá en Java la siguiente 
+  situación:
+  Una cuenta bancaria de tipo caja de ahorros le pertenece a Fulano Gomez, 
+  nacido el 16/04/1990 y otra de tipo cuenta corriente le pertenece a 
+  Mengana Torres,nacida el 23/11/1991. 
+  Ambos están casados y viven juntos en Av. Triunvirato 3174, Villa Ortúzar.
+  Supongamos que Fulano y Mengana se mudan a nuevo hogar: ¿Hay que cambiar el 
+  domicilio de cada uno o basta con cambiar uno de los dos?
+*/
+
+package guia01.ejercicio11;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class Principal {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
