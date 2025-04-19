7a09ca730946f334de65b0b6801a29f4076b71bb
vortigano <44713214+vortigano@users.noreply.github.com>
2025-04-18 23:29:37 -0300

    G01_E07 ADD Principal.java y enunciado

---
+++ src/guia01/ejercicio07/Principal.java
@@ -0,0 +1,31 @@
+/*
+  7) Se precisa un robot (que tiene nombre) que permita atender llamadas 
+    telefónicas. La compañía puede detectar algunos clientes según su número de
+    teléfono, sin embargo, en otros casos no. Por ello, el robot debe ser capaz
+    de procesar alguno de los siguientes métodos homónimos:
+    ▪ saludar(): void
+    Emite por consola un saludo diciendo: "Hola, mi nombre es ___. ¿En qué
+    puedo ayudarte?".
+    ▪ saludar(Persona): void
+    Emite por consola un saludo diciendo: "Hola ___, mi nombre es ___. ¿En qué 
+    puedo ayudarte?".
+  Modelá la clase Robot en Java. Luego, invocá varias veces el método saludar a
+  través del método main de la clase Principal con diferentes variantes para ver
+  si saluda como corresponde.
+*/
+package guia01.ejercicio07;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class Principal {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Ejercicio 07");
+  }
+  
+}
