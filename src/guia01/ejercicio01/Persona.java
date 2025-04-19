9116f7419c1c9a0774f2b2df7e3c2e38db9f85bc
vortigano <44713214+vortigano@users.noreply.github.com>
2025-04-19 15:59:08 -0300

    G01_E09 UPDATE G01_E01 Persona con atributo Fecha

--- src/guia01/ejercicio01/Persona.java
+++ src/guia01/ejercicio01/Persona.java
@@ -1,20 +1,34 @@
 package guia01.ejercicio01;
 
+import guia01.ejercicio04.Fecha;
 import java.time.Year;
 
 public class Persona {
   private String nombre;
   private String apellido;
-  private int añoDeNacimiento;
+  private Fecha fechaDeNacimiento;
   private static final int AÑO_MINIMO = 1900;
+  // podrían ir como atributo de fecha luego
+  private static final int FECHA_DIA_POR_DEFECTO = 1;
+  private static final int FECHA_MES_POR_DEFECTO = 1;
   
-  public Persona(){};
+  public Persona(){
+    setFechaDeNacimiento();
+  };
   
-  public Persona(String nombre, String apellido, int añoDeNacimiento)
+  public Persona(String nombre, String apellido, int anyoDeNacimiento)
   {
     setNombre(nombre);
     setApellido(apellido);
-    setAñoDeNacimiento(añoDeNacimiento);
+    setFechaDeNacimiento();
+    setAñoDeNacimiento(anyoDeNacimiento);
+  }
+  
+  public Persona(String nombre, String apellido, Fecha fechaDeNacimiento)
+  {
+    setNombre(nombre);
+    setApellido(apellido);
+    this.fechaDeNacimiento = fechaDeNacimiento;
   }
   
   // devolver el nombre completo de la persona
@@ -32,7 +46,7 @@ public class Persona {
       System.out.println("  ESTADO  ");
       System.out.println("  Nombre              : " + nombre);
       System.out.println("  Apellido            : " + apellido);
-      System.out.println("  Anyo de Nacimiento  : " + añoDeNacimiento);
+      System.out.println("  Anyo de Nacimiento  : " + fechaDeNacimiento.getAnio());
       borde = borde.replaceAll(".", "-");
       System.out.println(borde);
   }
@@ -45,7 +59,8 @@ public class Persona {
   // Obtener su edad actual
   public int getEdad()
   {
-      return (getAñoActual() - añoDeNacimiento);
+      //return (getAñoActual() - añoDeNacimiento);
+    return (getAñoActual() - fechaDeNacimiento.getAnio());
   }
   
   // Saber si es mayor que una edad dada.
@@ -87,8 +102,21 @@ public class Persona {
   public void setAñoDeNacimiento(int añoDeNacimiento)
   {
       if(añoDeNacimiento > AÑO_MINIMO && añoDeNacimiento < getAñoActual())
-        this.añoDeNacimiento = añoDeNacimiento;
+        this.fechaDeNacimiento.setAnyo(añoDeNacimiento);
       else
         System.err.println("Año de nacimiento ingresado no válido.");
+  }
+  
+  public void setFechaDeNacimiento()
+  {
+    fechaDeNacimiento = new Fecha(
+            FECHA_DIA_POR_DEFECTO,
+            FECHA_MES_POR_DEFECTO,
+            getAñoActual());
+  }
+  
+  public void setFechaDeNacimiento(Fecha fechaDeNacimiento)
+  {
+    this.fechaDeNacimiento = fechaDeNacimiento;
   }
 }
