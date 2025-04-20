1c3cc77e90ea85c371ddb5511b4ee9ea0ffbf813
vortigano <44713214+vortigano@users.noreply.github.com>
2025-04-20 00:32:31 -0300

    G01_E12 ADD ImpresoraMonocromatica, metodos y atributos minimos

---
+++ src/guia01/ejercicio12/ImpresoraMonocromatica.java
@@ -0,0 +1,15 @@
+package guia01.ejercicio12;
+
+/**
+ *
+ * @author Alberto Martín Capurro
+ */
+public class ImpresoraMonocromatica {
+  private boolean estaEncendia;
+  private int     cantidadDeHojasActual;
+  private double  nivelDeTintaNegra;
+  
+  //int nivelSegunCantCaracteres(int) {…}
+  //void recargarBandeja(int) {…}
+  //void imprimir(Documento) {…}
+}
