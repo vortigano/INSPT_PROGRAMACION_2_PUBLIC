4582ebd7dbf2630c02cb753c6bc74c620e0198e3
vortigano <44713214+vortigano@users.noreply.github.com>
2025-04-20 18:41:20 -0300

    G01_E13 REFACTOR elimina Persona generada y usar Persona de G01_E01 en TarjetaDeCredito

--- src/guia01/ejercicio13/TarjetaDeCredito.java
+++ src/guia01/ejercicio13/TarjetaDeCredito.java
@@ -1,15 +1,22 @@
 package guia01.ejercicio13;
 
-/**
-* Código generado por la app UXFtoJava by Charly Cimino
-* @see https://github.com/CharlyCimino/uxf-to-java
-*/
+import guia01.ejercicio01.Persona;
+
 public class TarjetaDeCredito {
 
     private String entidadBancaria;
-    private String numero;
+    private String nroTarjeta;
     private double saldo;
     private EntidadFinanciera entidadFinanciera;
     private Persona titular;
 
+  public TarjetaDeCredito(String entidadBancaria, String nroTarjeta, double saldo, EntidadFinanciera entidadFinanciera, Persona titular) {
+    this.entidadBancaria = entidadBancaria;
+    this.nroTarjeta = nroTarjeta;
+    this.saldo = saldo;
+    this.entidadFinanciera = entidadFinanciera;
+    this.titular = titular;
+  }
+
+    
 }
