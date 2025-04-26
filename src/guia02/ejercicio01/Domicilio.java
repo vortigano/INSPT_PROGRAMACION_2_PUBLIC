e06c3ff3fbad75b0d6e53b31c9fa5d3a5b9cd203
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-04-26 17:21:41 -0300

    G02_E01 clases y tests iniciales adaptados de la clase del profesor

---
+++ src/guia02/ejercicio01/Domicilio.java
@@ -0,0 +1,27 @@
+package guia02.ejercicio01;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Domicilio {
+    private String calle;
+    private int numero;
+    private String barrio;
+
+    public Domicilio(String calle, int numero, String barrio) {
+        // Este constructor debería llamar a los setters asociados a cada atributo...
+        this.calle = calle;
+        this.numero = numero;
+        this.barrio = barrio;
+    }
+
+    @Override
+    public String toString() {
+        return "Domicilio{" + "calle=" + calle + ", numero=" + numero + ", barrio=" + barrio + '}';
+    }
+    
+    
+}
