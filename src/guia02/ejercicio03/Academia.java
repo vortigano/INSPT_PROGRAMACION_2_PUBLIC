cf091f59854b3d0986d57e28529af08ea3db7e57
vortigano <44713214+vortigano@users.noreply.github.com>
2025-05-23 16:03:32 -0300

    G02_E03 UPDATE: Academia.imprimirListadoDeCursos y constructor

--- src/guia02/ejercicio03/Academia.java
+++ src/guia02/ejercicio03/Academia.java
@@ -10,6 +10,11 @@ public class Academia {
 
     private ArrayList<Curso> cursos;
 
+    public Academia()
+    {
+      cursos = new ArrayList<Curso>();
+    }
+    
     public void inscribirAlumno(String cursoCodigo, String alumnoNombre, String dni) {
         // Método a resolver...
     }
@@ -22,9 +27,23 @@ public class Academia {
         // Método a resolver...
     }
 
-    public void imprimirListadoDeCursos() {
-        // Método a resolver...
+  public void imprimirListadoDeCursos()
+  {
+    if(!cursos.isEmpty())
+    {
+      for (Curso curso : cursos) {
+        System.out.println(
+          curso.getCodigo() + " " + 
+          curso.getTitulo() + " " +
+          curso.getHoras() + " Horas");
+      }
     }
+    else
+    {
+      System.out.println("No hay cursos en la academia");
+    }
+    
+  }
 
     public void imprimirAlumnosPorCurso(String cursoCodigo) {
         // Método a resolver...
