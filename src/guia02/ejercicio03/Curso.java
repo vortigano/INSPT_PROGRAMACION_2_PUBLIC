2d81aabcb2afbf82d389448bc2b4a009568b05cf
vortigano <44713214+vortigano@users.noreply.github.com>
2025-05-23 15:28:06 -0300

    G02_E03 UPDATE: Curso add constructor, getters y setters parcial

--- src/guia02/ejercicio03/Curso.java
+++ src/guia02/ejercicio03/Curso.java
@@ -1,6 +1,7 @@
 package guia02.ejercicio03;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * 
@@ -9,12 +10,54 @@ import java.util.ArrayList;
 public class Curso {
 
     private int               horas;
-    private String            CODIGO_ALFA_NUMERICO;
+    private String            codigoAlfaNumerico;
     private String            titulo;
     private ArrayList<Alumno> alumnos;
+    
+    public static final int HORAS_MAX       = 12;
+    public static final int HORAS_MIN       =  1;
+    public static final String SIN_TITULO   = "Sin titulo";
 
+    public Curso(int horas, String titulo, String codigoAlfaNumerico) {
+      this.codigoAlfaNumerico = codigoAlfaNumerico;
+      setHoras(horas);
+      setTitulo(titulo);
+      this.alumnos            = new ArrayList<Alumno>();
+    }
+    
+    public int getHoras()
+    {
+      return horas;
+    }
+    
+    public String getTitulo()
+    {
+      return titulo;
+    }
+
+    private void setHoras(int horas)
+    {
+      if(horas > HORAS_MAX)
+        horas = HORAS_MAX;
+      else if(horas < HORAS_MIN)
+        horas = HORAS_MIN;
+      this.horas = horas;
+    }
+    
+    private void setTitulo(String titulo)
+    {
+      if(titulo!=null && !titulo.isBlank())
+      {
+        this.titulo = titulo;
+      }
+      else
+      {
+        this.titulo = SIN_TITULO;
+      }
+    }
+    
     public ArrayList<Alumno> buscarAlumnosPorEncimaDe(double promedio) {
-        // Método a resolver...
+        
         return null;
     }
 
