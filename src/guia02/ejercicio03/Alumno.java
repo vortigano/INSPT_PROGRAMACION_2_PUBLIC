3be14ca98af92022c1f1bc25f7583d1dd9ac9963
vortigano <44713214+vortigano@users.noreply.github.com>
2025-05-22 22:36:06 -0300

    G02_E03 UPDATE: Alumno y Evaluacion, validaciones y encapsulamiento

--- src/guia02/ejercicio03/Alumno.java
+++ src/guia02/ejercicio03/Alumno.java
@@ -11,10 +11,58 @@ public class Alumno {
     private String                nombre;
     private String                DNI;
     private ArrayList<Evaluacion> evaluaciones;
+    
+    public static final String SIN_NOMBRE = "Sin nombre";
+    public static final String SIN_DNI    = "Sin dni";
 
-    public double calcularPromedio() {
-        // Método a resolver...
-        return 0;
+    public Alumno(String nombre, String DNI)
+    {
+      setNombre(nombre);
+      setDNI(DNI);
+    }
+    
+    public void setNombre(String nombre)
+    {
+      if(nombre != null && !nombre.isBlank())
+      {
+        this.nombre = nombre;
+      }
+      else
+      {
+        this.nombre = SIN_NOMBRE;
+      }
+    }
+    
+    public String getNombre()
+    {
+      return nombre;
+    }
+    
+    public String getDNI()
+    {
+      return DNI;
+    }
+    
+    public void setDNI(String DNI)
+    {
+      if (DNI != null && !DNI.isBlank()) {
+        this.DNI = DNI;
+      } 
+      else 
+      {
+        this.DNI = SIN_DNI;
+      }
+    }
+    
+    public double calcularPromedio()
+    {
+        
+      return 0;
+    }
+    
+    public void agregarEvaluacion(double nota, TipoDeEvaluacion tipo)
+    {
+      this.evaluaciones.add(new Evaluacion(nota, tipo));
     }
 
 }
