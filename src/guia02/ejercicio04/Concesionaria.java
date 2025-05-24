b292a8d9643385a08eb48dedb8d09fd8aff663b3
vortigano <44713214+vortigano@users.noreply.github.com>
2025-05-24 00:56:19 -0300

    G02_E04 terminado Concesionaria

--- src/guia02/ejercicio04/Concesionaria.java
+++ src/guia02/ejercicio04/Concesionaria.java
@@ -6,17 +6,132 @@ package guia02.ejercicio04;
  */
 import java.util.ArrayList;
 
-public class Consecionaria {
+public class Concesionaria {
 
-    private String nombre;
-    private ArrayList<Vehiculo> listaDeVehiculos;
+  private String nombre;
+  private ArrayList<Vehiculo> listaDeVehiculos;
 
-    public void mostrarVehiculos(TipoDeVehiculo tipodevehiculo1) {
-        // Método a resolver...
+  public static final String SIN_NOMBRE = "SIN_NOMBRE";
+  
+  public Concesionaria(String nombre)
+  {
+    setNombre(nombre);
+    listaDeVehiculos = new ArrayList<Vehiculo>();
+  }
+  
+  public void mostrarVehiculos(TipoDeVehiculo tipo)
+  {
+    for (Vehiculo vehiculo : listaDeVehiculos)
+    {
+      if(vehiculo.esTipo(tipo))
+        System.out.println("Concesionaria: " + nombre + ", Patente: " + vehiculo.getPatente()+ ", Tipo: "+ tipo);
     }
+  }
 
-    public void borrarVehiculo(String string1) {
-        // Método a resolver...
+  public void borrarVehiculo(String patente)
+  {
+    Vehiculo  vehiculo  = null;
+    
+    vehiculo = getVehiculo(patente);
+    if(vehiculo != null)
+    {
+      listaDeVehiculos.remove(vehiculo);
+      System.out.println("Vehiculo borrado " + patente);
     }
+    else
+    {
+      System.out.println("Vehiculo no encontrado " + patente);
+    }
+    
+  }
+  
+  /*
+    Metodo auxiliar
+  */
+  public Vehiculo getVehiculo(String patente)
+  {
+    boolean   found     = false;
+    Vehiculo  vehiculo  = null;
+    //int       indice    = 0;
+    for(int i=0; i<listaDeVehiculos.size() && !found; i++)
+    {
+      vehiculo = listaDeVehiculos.get(i);
+      if(vehiculo.getPatente().equalsIgnoreCase(patente))
+      {
+        found   = true;
+        //indice  = i;
+      }
+    }
+    
+    /*if(found)
+    {
+      listaDeVehiculos.remove(indice);
+      System.out.println("Vehiculo borrado " + patente);
+    }*/
+    return found ? vehiculo : null;
+  }
+  
+  /**
+  * Metodo auxiliar al enunciado
+  */
+  public int getCantidadVehiculos()
+  {
+    return listaDeVehiculos.size();
+  }
+  
+  /**
+  * Metodo auxiliar al enunciado
+  */
+  public boolean agregarVehiculo(String patente, double precio, TipoDeVehiculo tipo)
+  {
+    boolean agregado = false;
+    if(!existeVehiculo(patente))
+    {
+      Vehiculo vehiculo = new Vehiculo(patente, precio, tipo);
+      listaDeVehiculos.add(vehiculo);
+      agregado = true;
+    }
+    return agregado;
+  }
+  
+  /**
+   * Metodo auxiliar al enunciado
+   */
+  public boolean existeVehiculo(String patente)
+  {
+    boolean existe = false;
+    for(int i=0; i<listaDeVehiculos.size() && !existe; i++)
+    {
+      Vehiculo vehiculo = listaDeVehiculos.get(i);
+      if(vehiculo.getPatente().equalsIgnoreCase(patente))
+      {
+        existe = true;
+      }
+    }
+    return existe;
+  }
+  
+  /**
+  * Metodo auxiliar al enunciado
+  */
+  private void setNombre(String nombre)
+  {
+    if(nombre == null || nombre.isBlank())
+    {
+      this.nombre = SIN_NOMBRE;
+    }
+    else
+    {
+      this.nombre = nombre;
+    }
+  }
+  
+  /**
+  * Metodo auxiliar al enunciado
+  */
+  public String getNombre()
+  {
+    return nombre;
+  }
 
 }
--- src/guia02/ejercicio04/Vehiculo.java
+++ src/guia02/ejercicio04/Vehiculo.java
@@ -6,8 +6,57 @@ package guia02.ejercicio04;
  */
 public class Vehiculo {
 
-    private String patente;
-    private double precio;
-    private TipoDeVehiculo tipo;
-
+    private String              patente;
+    private double              precio;
+    private TipoDeVehiculo      tipo;
+    
+    public static final double  PRECIO_MINIMO  = 1;
+    public static final String  SIN_PATENTE    = "SINPAT";
+    
+    public Vehiculo(String patente, double precio, TipoDeVehiculo tipo)
+    {
+      setPatente(patente);
+      setPrecio(precio);
+      this.tipo = tipo;
+    }
+    
+    private void setPatente(String patente)
+    {
+      if(patente==null || patente.isBlank())
+      {
+        System.out.println("Patente invalida se asigna " + SIN_PATENTE);
+      }
+      else
+      {
+        this.patente = patente;
+      }
+    }
+    
+    private void setPrecio(double precio)
+    {
+      if(precio > PRECIO_MINIMO)
+        this.precio = precio;
+      else
+      {
+        System.out.println("Precio de vehiculo por debajo del minimo. Se asigna precio minimo "+ PRECIO_MINIMO );
+        this.precio = PRECIO_MINIMO;
+      }
+        
+    }
+    
+    public String getPatente()
+    {
+      return patente;
+    }
+    
+    public double getPrecio()
+    {
+      return precio;
+    }
+    
+    public boolean esTipo(TipoDeVehiculo tipo)
+    {
+      return this.tipo == tipo;
+    }
+    
 }
