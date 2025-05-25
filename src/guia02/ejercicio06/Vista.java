8201d1397f83472b5405819e40c9e0c5e4bf93fa
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-05-25 18:47:57 -0300

    G02_E06 UPDATE: Vista

--- src/guia02/ejercicio06/Vista.java
+++ src/guia02/ejercicio06/Vista.java
@@ -7,7 +7,55 @@ package guia02.ejercicio06;
 public class Vista {
 
     private String clienteDni;
-    private int peliculaCodigo;
+    private String peliculaNombre;
     private int visualizaciones;
+    
+    public Vista(String clienteDni, String peliculaNombre)
+    {
+      this.clienteDni     = clienteDni;
+      this.peliculaNombre = peliculaNombre;
+      visualizaciones     = 1;
+    }
+    
+    public int getVisualizaciones()
+    {
+      return visualizaciones;
+    }
+    
+    public void addVisualizacion(int visualizacion)
+    {
+      visualizaciones += visualizacion;
+    }
+    
+    public void incVisualizacion()
+    {
+      visualizaciones ++;
+    }
+    
+    public boolean esMismoDni(String dni)
+    {
+      return (this.clienteDni.equalsIgnoreCase(dni));
+    }
+    
+    public boolean equalClienteDniYPelicula(String dni, String peliculaNombre)
+    {
+      boolean esIgual = false;
+      if(dni!=null && peliculaNombre!=null)
+      {
+        if(dni.equalsIgnoreCase(this.clienteDni) && peliculaNombre.equals(this.peliculaNombre))
+        {
+          esIgual = true;
+        }
+      }
+        
+      return esIgual;
+    }
+
+  @Override
+  public String toString() {
+    return String.format("\nCliente %s  Pelicula %s  Visualizaciones %d", clienteDni, peliculaNombre, visualizaciones);
+  }
+    
+    
 
 }
