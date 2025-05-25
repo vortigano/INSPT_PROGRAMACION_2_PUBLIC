1708426073691ca0ec9759872e8eb8765c0a18b2
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-05-25 18:47:50 -0300

    G02_E06 UPDATE: Pelicula

--- src/guia02/ejercicio06/Pelicula.java
+++ src/guia02/ejercicio06/Pelicula.java
@@ -6,8 +6,29 @@ package guia02.ejercicio06;
  */
 public class Pelicula {
 
-    private int             codigo;
     private String          nombre;
     private TipoDeServicio  tipoDeServicio;
 
+    public Pelicula(String nombre, TipoDeServicio tipo)
+    {
+      this.nombre         = nombre;
+      this.tipoDeServicio = tipo;
+    }
+    
+    public String getNombre()
+    {
+      return nombre;
+    }
+    
+    public TipoDeServicio getTipoDeServicio()
+    {
+      return tipoDeServicio;
+    }
+
+  @Override
+  public String toString() {
+    return String.format("\nPelicula %s, Servicio %s", nombre, tipoDeServicio);
+  }
+    
+    
 }
