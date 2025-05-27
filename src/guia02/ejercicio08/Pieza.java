a2c3c6e6123580889e13b14bfb612ac6fb24d165
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2025-05-26 21:32:40 -0300

    G02_E08 UPDATE: Pieza

--- src/guia02/ejercicio08/Pieza.java
+++ src/guia02/ejercicio08/Pieza.java
@@ -6,13 +6,47 @@ package guia02.ejercicio08;
  */
 public class Pieza {
 
-    public static final double  SALUDABLE_MIN = 50;
-    private TipoPieza           tipo;
-    private double              salud;
+  public static final double  SALUDABLE_MIN = 50;
+  public static final double  SALUD_NUEVO   = 100;
+  public static final double  DESGASTE_MAX  = 100;
+  public static final double  DESGASTE_MIN  = 0;
+  private TipoPieza           tipo;
+  private double              salud;
 
-    public boolean esSaludable() {
-        // Método a resolver...
-        return false;
+  public Pieza(TipoPieza tipo)
+  {
+    this.tipo   = tipo;
+    this.salud  = 100;
+  }
+  
+  public TipoPieza getTipo()
+  {
+    return tipo;
+  }
+
+  public boolean esSaludable()
+  {
+    return (salud >= SALUDABLE_MIN);
+  }
+  
+  void desgaste(double porcentajeDeDesgaste)
+  {
+    if(porcentajeDeDesgaste > DESGASTE_MAX)
+    {
+      porcentajeDeDesgaste = DESGASTE_MAX;
     }
+    else if(porcentajeDeDesgaste < DESGASTE_MIN)
+    {
+      porcentajeDeDesgaste = DESGASTE_MIN;
+    }
+    
+    this.salud *= ((100 - porcentajeDeDesgaste) * 0.01);
+    
+  }
 
+  @Override
+  public String toString() {
+    return String.format("\nPIEZA: %24s    SALUD: %6.1f   %s", tipo.getNombre(), salud, (esSaludable())?("-"):("ROTO"));
+  }
+   
 }
